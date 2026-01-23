f52ff9b4ecc442f73d82898546ef704a17b45a25
Vatsal Chandrani <vasuchandrani1118@gmail.com>
2026-01-23 22:48:49 +0530

    init

---
+++ cityshop-backend/src/main/java/com/cityshop/cityshopbackend/CityshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cityshop.cityshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityshopBackendApplication.class, args);
+    }
+
+}
---
+++ cityshop-backend/src/test/java/com/cityshop/cityshopbackend/CityshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityshop.cityshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
